0380389e5f0e734e0ae85c35944fe28d2a51563a
Nadja <nadja.chernygina@gmail.com>
2020-07-25 19:16:29 +0300

    hometask-03 functions

--- 03-homework/script.js
+++ 03-homework/script.js
@@ -5,4 +5,142 @@ function getMaxDigit(numbers){
     return maxNumber;  
 }
 
-document.writeln(`Функція №1: ${getMaxDigit(121212136)}`)
+document.writeln(`<p><b>Функція, яка отримує будь-яке число та виводить найбільшу цифру:</b> ${getMaxDigit(121212136)} </p>`);
+
+
+
+
+function getDegree() {
+    let number1 = parseInt(prompt('Введіть число, яке хочете піднести до ступеня'));
+
+    while (isNaN(number1)) {
+        alert('це число нам не підходить, треба ЦІЛЕ число');
+        number1 = parseInt(prompt('Введіть число, яке хочете піднести до ступеня'));
+    }
+
+    let number2 = parseInt(prompt('Введіть ступінь'));
+    
+    while (isNaN(number2) || (number2 < 1)) {
+        alert('це число нам не підходить, треба ЦІЛЕ число, а також не меньш ніж 1 ');
+        number2 = parseInt(prompt('Введіть ступінь'));
+    }
+
+    let result = number1;
+
+    for (let i = 1; i < number2; i++) {
+        result *= number1;
+    }
+
+    return result;
+}
+
+
+
+document.writeln(`<p><b>Функція, яка визначає ступінь числа:</b> ${getDegree()}</p>`);
+
+
+
+function formatName() {
+    let myName = String(prompt('Введіть ваше імя')).toLowerCase();
+
+    let modifiedName =  myName.charAt(0).toUpperCase() + myName.slice(1); 
+
+
+    return modifiedName;
+}
+
+
+document.writeln(`<p><b>Функція, яка форматує ім'я, роблячи першу букву великою:</b> ${formatName()}</p>`);
+
+function amountPayment(salary) {
+    const tax = 19.5;
+    const taxSalary = (tax*salary)/100;
+    const netSalary = salary - taxSalary;
+
+    return netSalary;
+}
+
+document.writeln(`<p><b>Функцію, яка вираховує суму, що залишається після оплати податку від зарабітньої плати:</b> ${amountPayment(1500)}</p>`);
+
+function getRandomNumber(n, m) {
+    const randomNum = Math.floor(n + Math.random()*(m + 1 - n));
+
+    return randomNum;
+}
+
+document.writeln(`<p><b>Функція, яка повертає випадкове ціле число в діапазоні від N до M:</b> ${getRandomNumber(1, 700)}</p>`);
+
+
+function countLetter(letter, word) {
+    let count = 0;
+
+    for (let i = 0; i < word.length; i++ ) {
+        if ( word.charAt(i).toLowerCase() === letter) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+document.writeln(`<p><b>Функція, яка рахує скільки разів певна буква повторюється в слові:</b> ${countLetter("r", "America")}</p>`);
+
+
+function convertCurrency(money) {
+    const modifiedMoney = money.toLowerCase();
+
+    if (modifiedMoney.indexOf("uah") !== -1) {
+        const convert1 = modifiedMoney.replace('uah'," ")/27;
+ 
+        document.writeln(` $:  ${convert1}`);
+
+    } else if (modifiedMoney.indexOf("$") !== -1) {
+        const convert2 = modifiedMoney.replace('$'," ")*27;
+
+        document.writeln(` UAH:  ${convert2}`);
+
+    } else {
+        alert("not correct money!");
+    }
+
+    return modifiedMoney;
+}
+
+convertCurrency("10$");
+
+
+function getRandomPassword(passLength) {
+    const numbers = "1234567890";
+    let randomstring = '';
+
+
+    if (!isNaN(passLength) && passLength > 0 && passLength <= 8) {
+         for (let i = 0; i< passLength; i++) {
+
+            let randomNum = Math.floor(Math.random() * numbers.length);
+            randomstring += numbers.substring(randomNum,randomNum+1);
+        }
+
+    
+    } else {
+        alert("not correct number");
+    }
+
+    return randomstring;
+    
+}
+
+document.writeln(`<p><b>Функцію генерації випадкового паролю:</b> ${getRandomPassword(6)}</p>`);
+
+
+function deleteLetters(letter, word) {
+    let modifiedWord = word.toLowerCase();
+
+    for (let i = 0; i < word.length; i++ ) {
+        ourWord = modifiedWord.replace(letter, '');
+    }
+
+    return ourWord;
+}
+
+document.writeln(`<p><b>Функція, яка видаляє всі букви з речення:</b> ${deleteLetters("m", "America")}</p>`);
