10c2f17a86f02dcb7cb6bf6bc8c52aefc178dab4
Nadja <nadja.chernygina@gmail.com>
2020-08-05 21:45:08 +0300

    added HW#7

---
+++ 07-homework/script.js
@@ -0,0 +1,42 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+
+// task #1
+function getMyTaxes(salary) {
+  return this.tax * salary;
+}
+
+console.log(getMyTaxes.call(ukraine, 24000));
+
+// task #2
+function getMiddleTaxes(country) {
+  return this.tax * this.middleSalary;
+}
+
+console.log(getMiddleTaxes.call(ukraine));
+
+// task #3
+function getTotalTaxes(country) {
+  return this.tax * this.middleSalary * this.vacancies;
+}
+
+console.log(getTotalTaxes.call(latvia));
+
+
+// task #4
+function getMySalary(country) {
+  let salary = Math.round(1500 + Math.random() * (2000 - 1500));
+  let taxes = Math.round(this.tax * salary);
+  let profit = Math.round(salary - taxes);
+
+  return { 
+    salary,
+    taxes,
+    profit
+  };
+}
+
+console.log(getMySalary.call(latvia));
+const result = setInterval(() => console.log(getMySalary.call(litva)), 10000);
